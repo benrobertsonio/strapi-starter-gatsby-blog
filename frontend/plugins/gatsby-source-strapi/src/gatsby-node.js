bb6b9c1bc4acc8628babe9f290816cf616bb75a8
Maxime Castres <maximecastres@macbook-pro-de-maxime.home>
2020-02-18 19:37:03 +0100

    Gatsby preview tests

---
+++ frontend/plugins/gatsby-source-strapi/src/gatsby-node.js
@@ -0,0 +1,94 @@
+import axios from 'axios'
+import fetchData from './fetch'
+import { Node } from './nodes'
+import { capitalize } from 'lodash'
+import normalize from './normalize'
+import authentication from './authentication'
+
+exports.sourceNodes = async (
+  {
+    store,
+    actions,
+    cache,
+    reporter,
+    createContentDigest,
+    createNodeId,
+    getNode,
+    getNodes,
+    getNodesByType,
+  },
+  {
+    apiURL = 'http://localhost:1337',
+    contentTypes = [],
+    loginData = {},
+    queryLimit = 100,
+  }
+) => {
+  const { createNode, deleteNode, touchNode } = actions
+
+  const existingNodes = getNodes().filter(
+    n => n.internal.owner === `gatsby-source-strapi`
+  )
+
+  existingNodes.forEach(n => {
+    touchNode({ nodeId: n.id })
+  })
+
+  // Authentication
+  let jwtToken = await authentication({ loginData, reporter, apiURL })
+
+  // Start activyti, Strapi data fetching
+  const fetchActivity = reporter.activityTimer(`Fetching Strapi Data`)
+  fetchActivity.start()
+
+  // Generate a list of promises based on the `contentTypes` option.
+
+  const promises = contentTypes.map(contentType =>
+    fetchData({
+      apiURL,
+      contentType,
+      jwtToken,
+      queryLimit,
+      reporter,
+    })
+  )
+
+  // Execute the promises.
+  let entities = await Promise.all(promises)
+
+  entities = await normalize.downloadMediaFiles({
+    entities,
+    apiURL,
+    store,
+    cache,
+    createNode,
+    touchNode,
+    jwtToken,
+  })
+
+  // Create nodes
+  contentTypes.forEach((contentType, i) => {
+    const items = entities[i]
+    items.forEach((item, i) => {
+      const node = Node(capitalize(contentType), item)
+      createNode(node)
+    })
+  })
+
+  const newNodes = getNodes().filter(
+    n => n.internal.owner === `gatsby-source-strapi`
+  )
+
+  const diff = existingNodes.filter(
+    ({ id: id1 }) => !newNodes.some(({ id: id2 }) => id2 === id1)
+  )
+
+  // Delete nodes
+  diff.forEach(data => {
+    deleteNode({ node: data.id })
+  })
+
+  console.log(diff)
+
+  fetchActivity.end()
+}
